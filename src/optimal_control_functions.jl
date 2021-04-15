2c2a88e69f2951f8959a800315d880647b4ded8d
tmigot <tangi.migot@gmail.com>
2021-04-15 02:17:20 +0200

    up control functions

--- src/optimal_control_functions.jl
+++ src/optimal_control_functions.jl
@@ -1,3 +1,36 @@
+export bounded_2d_variational_problem
+
+function bounded_2d_variational_problem(n, domain, x0, xf, xmin, xmax, f)
+  
+    partition = n
+    model = CartesianDiscreteModel(domain,partition)
+      
+    labels = get_face_labeling(model)
+    add_tag_from_tags!(labels,"diri1",[2])
+    add_tag_from_tags!(labels,"diri0",[1])
+  
+    V0 = TestFESpace(reffe=:Lagrangian, order=1, valuetype=Float64,
+                           conformity=:H1, model=model, dirichlet_tags=["diri0","diri1"])
+    V1 = TestFESpace(reffe=:Lagrangian, order=1, valuetype=Float64,
+                           conformity=:H1, model=model, dirichlet_tags=["diri0","diri1"])
+  
+    U0  = TrialFESpace(V0, [x0[1], xf[1]])
+    U1  = TrialFESpace(V0, [x0[2], xf[2]])
+  
+    V = MultiFieldFESpace([V0, V1])
+    U = MultiFieldFESpace([U0, U1])
+    nU0 = Gridap.FESpaces.num_free_dofs(U0)
+    nU1 = Gridap.FESpaces.num_free_dofs(U1)
+  
+    trian = Triangulation(model)
+    degree = 1
+    quad = CellQuadrature(trian, degree)
+  
+    xin = zeros(nU0 + nU1)
+    nlp = GridapPDENLPModel(xin, f, trian, quad, U, V, lvar = xmin * ones(nU0+nU1), uvar = xmax*ones(nU0+nU1))
+    return nlp
+end
+
 export GridapOptimalControlNLPModel
 #Works for 1d state and control only
 function GridapOptimalControlNLPModel(f, con, domain, partition; x0 = nothing, xf = nothing, umin = nothing, umax = nothing)
@@ -86,8 +119,23 @@ function adjoint_function_final_condition(domain, partition, mul, xf)
   return vcat(ans[1:end-1], xf)
 end
 
+function adjoint_function(domain, partition, mul)
+    model = CartesianDiscreteModel(domain,partition)
+    labels = get_face_labeling(model)
+    add_tag_from_tags!(labels,"diri0",[1])
+    add_tag_from_tags!(labels,"diri1",[2])
+    V0 = TestFESpace(reffe=:Lagrangian, order=1, valuetype=Float64,
+                             conformity=:H1, model=model, dirichlet_tags=["diri0", "diri1"])
+    U = TrialFESpace(V0, [0., 0.])
+    ph = FEFunction(U, mul)
+    midpoint(xs) = sum(xs)/length(xs) #doesn't work on the last cell
+    cell_xm = apply(midpoint, ph.cell_vals)
+    ans = get_free_values(Gridap.FESpaces.interpolate(cell_xm, U))
+    return ans
+end
+
 export bounded_2D_optimal_control
-function bounded_2D_optimal_control(domain, n, f, con1, con2, umin, umax, xmin, xmax)
+function bounded_2D_optimal_control(domain, n, f, con1, con2, umin, umax, xmin, xmax, x0, xf)
   
     partition = n
     model = CartesianDiscreteModel(domain,partition)
@@ -98,10 +146,10 @@ function bounded_2D_optimal_control(domain, n, f, con1, con2, umin, umax, xmin, 
   
     V01 = TestFESpace(reffe=:Lagrangian, order=1, valuetype=Float64,
                            conformity=:H1, model=model, dirichlet_tags=["diri0","diri1"])
-    U1 = TrialFESpace(V01, [0., γ])
+    U1 = TrialFESpace(V01, [x0[1], xf[1]])
     V02 = TestFESpace(reffe=:Lagrangian, order=1, valuetype=Float64,
                            conformity=:H1, model=model, dirichlet_tags=["diri0"])
-    U2 = TrialFESpace(V02, [0.])
+    U2 = TrialFESpace(V02, [x0[2]])
     U = MultiFieldFESpace([U1, U2])
     V0 = MultiFieldFESpace([V01, V02])
     nU1, nU2 = Gridap.FESpaces.num_free_dofs(U1), Gridap.FESpaces.num_free_dofs(U2)
@@ -135,7 +183,7 @@ function bounded_2D_optimal_control(domain, n, f, con1, con2, umin, umax, xmin, 
     xin = zeros(nU + nUcon)
 
     #lvar, uvar = bounds_functions_to_vectors(MultiFieldFESpace([U1, U2, Ucon]), Ucon, U, trian, zeros(nU), vcat(γ*ones(nU1), ones(nU2)), umin, umax)
-    lvar, uvar = bounds_functions_to_vectors2(
+    lvar, uvar = bounds_functions_to_vectors(
         MultiFieldFESpace([U1, U2, Ucon]), 
         Ucon, U, trian, 
         xmin, xmax, umin, umax
