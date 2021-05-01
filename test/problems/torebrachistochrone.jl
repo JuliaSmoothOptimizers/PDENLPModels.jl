74171d60b46870ee5ab974b23e14c90417fc65cc
tmigot <tmigot@users.noreply.github.com>
2021-05-01 00:36:00 +0000

    :robot: Format .jl files

--- test/problems/torebrachistochrone.jl
+++ test/problems/torebrachistochrone.jl
@@ -3,14 +3,14 @@
 function torebrachistochrone(args...; n = 3, kwargs...)
   # n est la taille de la discrétisation (entier)
   # le domain au format (t₀, T)
-  domain = (0,1)
+  domain = (0, 1)
   # x0 est le vecteur des données initiales
   x0 = zeros(2)
   # xf est le vecteur des données finales
   xf = π * ones(2)
   # xmin et xmax sont des nombres qui représentent les bornes:
   xmin = 0
-  xmax = 2*π
+  xmax = 2 * π
   #La fonction objectif f:
   a = 1
   c = 3
@@ -22,60 +22,59 @@ function torebrachistochrone(args...; n = 3, kwargs...)
   end
 
   model = CartesianDiscreteModel(domain, n)
-      
+
   labels = get_face_labeling(model)
-  add_tag_from_tags!(labels,"diri1",[2])
-  add_tag_from_tags!(labels,"diri0",[1])
-  
+  add_tag_from_tags!(labels, "diri1", [2])
+  add_tag_from_tags!(labels, "diri0", [1])
+
   V0 = TestFESpace(
-    reffe=:Lagrangian, 
-    order=1, 
-    valuetype=Float64,
-    conformity=:H1, 
-    model=model, 
-    dirichlet_tags=["diri0","diri1"],
+    reffe = :Lagrangian,
+    order = 1,
+    valuetype = Float64,
+    conformity = :H1,
+    model = model,
+    dirichlet_tags = ["diri0", "diri1"],
   )
   V1 = TestFESpace(
-    reffe=:Lagrangian, 
-    order=1, 
-    valuetype=Float64,
-    conformity=:H1, 
-    model=model, 
-    dirichlet_tags=["diri0","diri1"],
+    reffe = :Lagrangian,
+    order = 1,
+    valuetype = Float64,
+    conformity = :H1,
+    model = model,
+    dirichlet_tags = ["diri0", "diri1"],
   )
-  
-  U0  = TrialFESpace(V0, [x0[1], xf[1]])
-  U1  = TrialFESpace(V0, [x0[2], xf[2]])
-  
+
+  U0 = TrialFESpace(V0, [x0[1], xf[1]])
+  U1 = TrialFESpace(V0, [x0[2], xf[2]])
+
   V = MultiFieldFESpace([V0, V1])
   U = MultiFieldFESpace([U0, U1])
   nU0 = Gridap.FESpaces.num_free_dofs(U0)
   nU1 = Gridap.FESpaces.num_free_dofs(U1)
-  
+
   trian = Triangulation(model)
   degree = 1
   quad = CellQuadrature(trian, degree)
-  
+
   return GridapPDENLPModel(
-    zeros(nU0 + nU1), 
-    f, 
-    trian, 
-    quad, 
-    U, 
-    V, 
-    lvar = xmin * ones(nU0+nU1), 
-    uvar = xmax * ones(nU0+nU1),
+    zeros(nU0 + nU1),
+    f,
+    trian,
+    quad,
+    U,
+    V,
+    lvar = xmin * ones(nU0 + nU1),
+    uvar = xmax * ones(nU0 + nU1),
   )
 end
 
 function torebrachistochrone_test()
-
   nlp = torebrachistochrone()
   stats = ipopt(nlp, print_level = 0)
 
-  nn = Int(nlp.nvar_pde/2)
+  nn = Int(nlp.nvar_pde / 2)
   φs = stats.solution[1:nn]
-  θs = stats.solution[nn+1:2*nn]
+  θs = stats.solution[(nn + 1):(2 * nn)]
 
   xs = (c .+ a * cos.(φs)) .* cos.(θs)
   ys = (c .+ a * cos.(φs)) .* sin.(θs)
