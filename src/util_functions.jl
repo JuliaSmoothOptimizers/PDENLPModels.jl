1f32b7f129d19722eda78c91aa88d8435456c7f9
tmigot <tangi.migot@gmail.com>
2021-10-21 21:52:26 +0200

    add `split_vectors`

--- src/util_functions.jl
+++ src/util_functions.jl
@@ -60,3 +60,44 @@ function _split_vector(x::AbstractVector, Ypde::FESpace, Ycon::VoidFESpace)
 
   return y, [], k
 end
+
+"""
+    `_split_vectors(x, Ypde, Ycon)`
+
+Take a vector x and returns a splitting in terms of `y`, `u` and `θ`.
+"""
+function _split_vectors(x::AbstractVector, Ypde::FESpace, Ycon::FESpace)
+  nvar_pde = Gridap.FESpaces.num_free_dofs(Ypde)
+  nvar_con = Gridap.FESpaces.num_free_dofs(Ycon)
+
+  nparam = length(x) - nvar_pde - nvar_con
+  θ = x[(nvar_pde + nvar_con + 1):end]
+
+  leng(S::MultiFieldFESpace, i) = sum([Gridap.FESpaces.num_free_dofs(S.spaces[j]) for j=1:i])
+  sum_old(S::MultiFieldFESpace, i) = if i == 1 
+    0
+  else 
+    sum([Gridap.FESpaces.num_free_dofs(S[j]) for j=1:i-1])
+  end
+
+  y = if typeof(Ypde) <: MultiFieldFESpace
+    (x[sum_old(Ypde, i)+1:leng(Ypde, i)] for i=1:length(Ypde.spaces))
+  else
+    x[1:nvar_pde]
+  end
+  if nvar_con == 0 && nparam == 0
+    return y
+  elseif nvar_con == 0 && nparam > 0
+    return y, θ
+  end
+  u = if typeof(Ycon) <: MultiFieldFESpace
+    (x[(sum_old(Ycon, i) + 1 + nvar_pde):leng(Ycon, i)] for i=1:length(Ycon.spaces))
+  else
+    x[(1 + nvar_pde):(nvar_pde + nvar_con)]
+  end
+  if nparam == 0
+    return y, u
+  end
+
+  return y, u, θ
+end
