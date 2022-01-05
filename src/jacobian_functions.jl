1577da801d2f51ca42865c0e7ae8973fac1fc4bc
tmigot <tangi.migot@gmail.com>
2022-01-05 21:13:20 +0100

    fix the jacobian build

--- src/jacobian_functions.jl
+++ src/jacobian_functions.jl
@@ -104,45 +104,71 @@ function _jacobian_struct(
   κ = @view x[1:nparam]
 
   v = Gridap.FESpaces.get_fe_basis(Xpde)
+  #du = Gridap.FESpaces.get_trial_fe_basis(Ypde)
 
-  du = Gridap.FESpaces.get_trial_fe_basis(Ypde)
   if nparam == 0
     if typeof(Ycon) <: VoidFESpace
       resuh = op.res(yh, v)
-      dcjacy = Gridap.FESpaces._jacobian(x -> op.res(x, du), yh, resuh)
+      dcjacy = Gridap.FESpaces._jacobian(x -> op.res(x, v), yh, resuh)
     else
       resuh = op.res(yh, uh, v)
-      dcjacy = Gridap.FESpaces._jacobian(x -> op.res(x, uh, du), yh, resuh)
+      dcjacy = Gridap.FESpaces._jacobian(x -> op.res(x, uh, v), yh, resuh)
     end
   else
     if typeof(Ycon) <: VoidFESpace
       resuh = op.res(κ, yh, v)
-      dcjacy = Gridap.FESpaces._jacobian(x -> op.res(κ, x, du), yh, resuh)
+      dcjacy = Gridap.FESpaces._jacobian(x -> op.res(κ, x, v), yh, resuh)
     else
       resuh = op.res(κ, yh, uh, v)
-      dcjacy = Gridap.FESpaces._jacobian(x -> op.res(κ, x, uh, du), yh, resuh)
+      dcjacy = Gridap.FESpaces._jacobian(x -> op.res(κ, x, uh, v), yh, resuh)
     end
   end
   matdata_y = Gridap.FESpaces.collect_cell_matrix(Ypde, Xpde, dcjacy)
   assem = SparseMatrixAssembler(Ypde, Xpde)
+  #= Gridap 0.15.5
   n = Gridap.FESpaces.count_matrix_nnz_coo(assem, matdata_y)
   Iy, Jy, Vy = Gridap.FESpaces.allocate_coo_vectors(Gridap.FESpaces.get_matrix_type(assem), n)
   ny = Gridap.FESpaces.fill_matrix_coo_numeric!(Iy, Jy, Vy, assem, matdata_y)
+  =#
+  m1 = Gridap.FESpaces.nz_counter(
+    Gridap.FESpaces.get_matrix_builder(assem),
+    (Gridap.FESpaces.get_rows(assem), Gridap.FESpaces.get_cols(assem)),
+  ) # Gridap.Algebra.CounterCS
+  Gridap.FESpaces.symbolic_loop_matrix!(m1, assem, matdata_y)
+  m2 = Gridap.FESpaces.nz_allocation(m1) # Gridap.Algebra.InserterCSC
+  Gridap.FESpaces.symbolic_loop_matrix!(m2, assem, matdata_y)
+  m3 = sparse(LowerTriangular(Gridap.FESpaces.create_from_nz(m2)))
+  Iy, Jy, _ = findnz(m3) # If I remember correctly, this is what I wanted to avoid...
+  # Gridap.FESpaces.numeric_loop_matrix!(m2, assem, matdata)
+  ny = length(Iy)
   Iy, Jy = Iy[1:ny], Jy[1:ny]
 
   if Ycon != VoidFESpace()
     if nparam == 0
       resuh = op.res(yh, uh, v)
-      dcjacu = _jacobian2(x -> op.res(yh, x, du), uh, resuh)
+      dcjacu = _jacobian2(x -> op.res(yh, x, v), uh, resuh)
     else
       resuh = op.res(κ, yh, uh, v)
-      dcjacu = _jacobian2(x -> op.res(κ, yh, x, du), uh, resuh)
+      dcjacu = _jacobian2(x -> op.res(κ, yh, x, v), uh, resuh)
     end
     matdata_u = Gridap.FESpaces.collect_cell_matrix(Ycon, Xpde, dcjacu)
     assem = SparseMatrixAssembler(Ycon, Xpde)
+    #=
     n = Gridap.FESpaces.count_matrix_nnz_coo(assem, matdata_u)
     Iu, Ju, Vu = Gridap.FESpaces.allocate_coo_vectors(Gridap.FESpaces.get_matrix_type(assem), n)
     nu = Gridap.FESpaces.fill_matrix_coo_numeric!(Iu, Ju, Vu, assem, matdata_u)
+    =#
+    m1 = Gridap.FESpaces.nz_counter(
+      Gridap.FESpaces.get_matrix_builder(assem),
+      (Gridap.FESpaces.get_rows(assem), Gridap.FESpaces.get_cols(assem)),
+    ) # Gridap.Algebra.CounterCS
+    Gridap.FESpaces.symbolic_loop_matrix!(m1, assem, matdata_u)
+    m2 = Gridap.FESpaces.nz_allocation(m1) # Gridap.Algebra.InserterCSC
+    Gridap.FESpaces.symbolic_loop_matrix!(m2, assem, matdata_u)
+    m3 = sparse(LowerTriangular(Gridap.FESpaces.create_from_nz(m2)))
+    Iu, Ju, _ = findnz(m3) # If I remember correctly, this is what I wanted to avoid...
+    # Gridap.FESpaces.numeric_loop_matrix!(m2, assem, matdata)
+    nu = length(Iu)
     Iu, Ju = Iu[1:nu], Ju[1:nu] .+ num_free_dofs(Ypde)
   else
     Iu, Ju, nu = Int[], Int[], 0
@@ -194,42 +220,73 @@ function _from_terms_to_jacobian_vals!(
   yh, uh = _split_FEFunction(xyu, Ypde, Ycon)
 
   v = Gridap.FESpaces.get_fe_basis(Xpde)
+  # du = Gridap.FESpaces.get_trial_fe_basis(Ypde)
 
-  du = Gridap.FESpaces.get_trial_fe_basis(Ypde)
   if nparam == 0
     if typeof(Ycon) <: VoidFESpace
       resuh = op.res(yh, v)
-      dcjacy = Gridap.FESpaces._jacobian(x -> op.res(x, du), yh, resuh)
+      dcjacy = Gridap.FESpaces._jacobian(x -> op.res(x, v), yh, resuh)
     else
       resuh = op.res(yh, uh, v)
-      dcjacy = Gridap.FESpaces._jacobian(x -> op.res(x, uh, du), yh, resuh)
+      dcjacy = Gridap.FESpaces._jacobian(x -> op.res(x, uh, v), yh, resuh)
     end
   else
     if typeof(Ycon) <: VoidFESpace
       resuh = op.res(κ, yh, v)
-      dcjacy = Gridap.FESpaces._jacobian(x -> op.res(κ, x, du), yh, resuh)
+      dcjacy = Gridap.FESpaces._jacobian(x -> op.res(κ, x, v), yh, resuh)
     else
       resuh = op.res(κ, yh, uh, v)
-      dcjacy = Gridap.FESpaces._jacobian(x -> op.res(κ, x, uh, du), yh, resuh)
+      dcjacy = Gridap.FESpaces._jacobian(x -> op.res(κ, x, uh, v), yh, resuh)
     end
   end
   matdata_y = Gridap.FESpaces.collect_cell_matrix(Ypde, Xpde, dcjacy)
   assem = SparseMatrixAssembler(Ypde, Xpde)
+  #= Gridap 0.15.5
   I, J = zeros(Int, length(vals)), zeros(Int, length(vals)) # nlp.Jrows, nlp.Jcols
   nini = Gridap.FESpaces.fill_matrix_coo_numeric!(I, J, vals, assem, matdata_y, nfirst)
+  =#
+  nini = nfirst
+  m1 = Gridap.FESpaces.nz_counter(
+    Gridap.FESpaces.get_matrix_builder(assem),
+    (Gridap.FESpaces.get_rows(assem), Gridap.FESpaces.get_cols(assem)),
+  ) # Gridap.Algebra.CounterCS
+  Gridap.FESpaces.symbolic_loop_matrix!(m1, assem, matdata_y)
+  m2 = Gridap.FESpaces.nz_allocation(m1) # Gridap.Algebra.InserterCSC
+  Gridap.FESpaces.symbolic_loop_matrix!(m2, assem, matdata_y)
+  m3 = sparse(LowerTriangular(Gridap.FESpaces.create_from_nz(m2)))
+  _, _, v = findnz(m3)
+  vals[(nini + 1):(nini + length(v))] .= v
+  nini += length(v)
+  # Gridap.FESpaces.numeric_loop_matrix!(m2, assem, matdata)
 
   if Ycon != VoidFESpace()
     if nparam == 0
       resuh = op.res(yh, uh, v)
-      dcjacu = _jacobian2(x -> op.res(yh, x, du), uh, resuh)
+      dcjacu = _jacobian2(x -> op.res(yh, x, v), uh, resuh)
     else
       resuh = op.res(κ, yh, uh, v)
-      dcjacu = _jacobian2(x -> op.res(κ, yh, x, du), uh, resuh)
+      dcjacu = _jacobian2(x -> op.res(κ, yh, x, v), uh, resuh)
     end
     matdata_u = Gridap.FESpaces.collect_cell_matrix(Ycon, Xpde, dcjacu)
     assem = SparseMatrixAssembler(Ycon, Xpde)
+    #=
     I, J = zeros(Int, length(vals)), zeros(Int, length(vals)) # nlp.Jrows, nlp.Jcols
     nini = Gridap.FESpaces.fill_matrix_coo_numeric!(I, J, vals, assem, matdata_u, nini)
+    =#
+    ##############################################################
+    # TO BE IMPROVED
+    m1 = Gridap.FESpaces.nz_counter(
+      Gridap.FESpaces.get_matrix_builder(assem),
+      (Gridap.FESpaces.get_rows(assem), Gridap.FESpaces.get_cols(assem)),
+    ) # Gridap.Algebra.CounterCS
+    Gridap.FESpaces.symbolic_loop_matrix!(m1, assem, matdata_u)
+    m2 = Gridap.FESpaces.nz_allocation(m1) # Gridap.Algebra.InserterCSC
+    Gridap.FESpaces.numeric_loop_matrix!(m2, assem, matdata_u)
+    m3 = sparse(LowerTriangular(Gridap.FESpaces.create_from_nz(m2)))
+    _, _, v = findnz(m3)
+    vals[(nini + 1):(nini + length(v))] .= v
+    nini += length(v)
+    ##############################################################
   else
     vals[(nini + 1):end] .= zero(T)
   end
